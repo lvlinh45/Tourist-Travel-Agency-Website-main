8951d2c2abaf99997c85fe2c96cc1e824148ed35
LuongVanLinh <luongvanlinh.45@gmail.com>
2025-06-23 15:58:00 +0700

    fix bug

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -76,14 +76,14 @@ function Packages() {
                   </p>
                   <div className="d-flex justify-content-center mb-2">
                     <a
-                      href="#"
+                      href="/"
                       className="btn btn-sm btn-primary px-3 border-end"
                       style={{ borderRadius: "30px 0 0 30px" }}
                     >
                       Read More
                     </a>
                     <a
-                      href="#"
+                      href="/"
                       className="btn btn-sm btn-primary px-3"
                       style={{ borderRadius: "0 30px 30px 0" }}
                     >
@@ -132,14 +132,14 @@ function Packages() {
                   </p>
                   <div className="d-flex justify-content-center mb-2">
                     <a
-                      href="#"
+                      href="/"
                       className="btn btn-sm btn-primary px-3 border-end"
                       style={{ borderRadius: "30px 0 0 30px" }}
                     >
                       Read More
                     </a>
                     <a
-                      href="#"
+                      href="/"
                       className="btn btn-sm btn-primary px-3"
                       style={{ borderRadius: "0 30px 30px 0" }}
                     >
@@ -188,14 +188,14 @@ function Packages() {
                   </p>
                   <div className="d-flex justify-content-center mb-2">
                     <a
-                      href="#"
+                      href="/"
                       className="btn btn-sm btn-primary px-3 border-end"
                       style={{ borderRadius: "30px 0 0 30px" }}
                     >
                       Read More
                     </a>
                     <a
-                      href="#"
+                      href="/"
                       className="btn btn-sm btn-primary px-3"
                       style={{ borderRadius: "0 30px 30px 0" }}
                     >
